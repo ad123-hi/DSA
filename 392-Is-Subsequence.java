1497d2cd31470022bab17e3bdd833ebd2c56222b
Aditya Kumar Gupta <152524305+ad123-hi@users.noreply.github.com>
2026-07-02 12:35:03 +0530

    [392] [Time Beats: 72.05%] [Memory Beats: 63.02%] - LeetPush

---
+++ 392-Is-Subsequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++; // move in s when match found
+            }
+            j++; // always move in t
+        }
+
+        return i == s.length(); // all chars in s matched
+    }
+}
